7f673c3f84310827168282891f4f2fdbf5563d5b
Lucas Rigo <lucas.rigo@numenit.com>
2025-06-09 11:17:03 -0300

    day3 part 2

--- day3/mullItOver.js
+++ day3/mullItOver.js
@@ -1,5 +1,6 @@
 var input = [
-`xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+`xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`,
+`xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
  ,puzzleInput
 ]
 
@@ -25,10 +26,22 @@ var part1 = function() {
 var part2 = function () {
 
   for (let i = 0; i < input.length; i++) {
-    const numberStrings = input[i].split(/\s+/)
-    const numbers = numberStrings.map((val => {return Number(val)}))
+    const matchs = input[i].match(/(mul\(\d{1,3}\,\d{1,3}\))|(do\(\))|(don\'t\(\))/g)
+    // console.log(matchs)
+    let total = 0
+    let enabled = true
+    matchs.forEach(str => {
+      if (str.startsWith('do(')) {
+        enabled = true
+      } else if (str.startsWith('don\'t(')) {
+        enabled = false
+      } else if (enabled && str.startsWith('mul(')) {
+        const digits = str.match(/\d+/g)
+        total += Number(digits[0]) * Number(digits[1])
+      }
+    })
 
-    const result = 0
+    const result = total
     // console.log(result)
     $('#part2').append(input[i])
       .append('<br>&emsp;')
