059194eb545989b26353c012b59c31f404aae7d4
Lucas Rigo <lucas.rigo@numenit.com>
2025-06-09 09:49:41 -0300

    day1 part 1

--- day1/historianHysteria.js
+++ day1/historianHysteria.js
@@ -1,15 +1,32 @@
 var input = [
-``
+`3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
  ,puzzleInput
 ]
 
 var part1 = function() {
 
   for (var i = 0; i < input.length; i++) {
-    var numberStrings = input[i].split(/\s+/)
-    var numbers = numberStrings.map((val => {return Number(val)}))
+    const listA = []
+    const listB = []
+    const rows = input[i].split(/\n/)
+    rows.forEach((row) => {
+      const numbers = row.split(/\s+/)
+      listA.push(Number(numbers[0]))
+      listB.push(Number(numbers[1]))
+    })
+    listA.sort((a, b) => a - b)
+    listB.sort((a, b) => a - b)
+    let totalDistance = 0
+    for (let j = 0; j < listA.length; j++) {
+      totalDistance += Math.abs(listA[j] - listB[j])
+    }
 
-    const result = 0
+    const result = totalDistance
     // console.log(result)
     $('#part1').append(input[i])
       .append('<br>&emsp;')
@@ -22,7 +39,7 @@ var part2 = function () {
 
   for (var i = 0; i < input.length; i++) {
     var numberStrings = input[i].split(/\s+/)
-    var numbers = numberStrings.map((val => {return Number(val)}))
+    var numbers = $.map(numberStrings, (val => {return Number(val)}))
 
     const result = 0
     // console.log(result)
